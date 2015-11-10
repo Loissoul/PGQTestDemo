0dcf3dd92c21d09093d1e6a0ae1f8ffac498a2fe
937542864@qq.com <937542864@qq.com>
2015-11-10 15:13:06 +0800

    Initial Commit

---
+++ PGQTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PGQTestDemo
+//
+//  Created by 魏帅 on 15/11/10.
+//  Copyright © 2015年 潘国强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PGQTestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PGQTestDemo
+//
+//  Created by 魏帅 on 15/11/10.
+//  Copyright © 2015年 潘国强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
